99baca6812029c3954531f17129eb92a2cff4a02
Tokunbo <toxylee20021@gmail.com>
2023-04-19 02:31:20 -0700

    Tasks

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
     Module 0-square
-    It defines an empty class called Square
+    Defines an empty class called Square.
 """
 
 
 class Square:
-    """This defines an empty Square class"""
+    """Defines an empty Square."""
     pass
