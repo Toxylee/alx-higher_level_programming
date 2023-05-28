fa4ada94f4a44791ac3b3399444a398c4f561d24
Tokunbo <toxylee20021@gmail.com>
2023-05-28 16:47:20 -0700

    Tasks

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes the score avagrage of all records in  second_table
+-- of the database hbtn_0c_0 in MySQL server.
+
+SELECT AVG(score) AS 'average' FROM second_table;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Remove or delete database if it only exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
