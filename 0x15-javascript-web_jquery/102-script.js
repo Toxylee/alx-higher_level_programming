b2e1fbc1c1794f521d582d2f29cb7f9f880bcd46
Toxylee <toxylee20021@gmail.com>
2023-08-21 16:22:44 -0700

    Jquery

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$('document').ready(()=> {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $('#btn_translate').on('click', ()=> {
+    $.get(url + $.param({ lang: $('#language_code').val() }), (data)=> {
+      $('#hello').text(data.hello);
+    });
+  });
+});
